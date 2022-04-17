10fa08c038e82b059f04d400d778e2153439223c
EnokeDeelaka <enokedeelakakulathunga@gmail.com>
2022-04-17 09:39:24 +0530

    3

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  parsedId: string | null = '';
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(response => {
+      this.parsedId = response.get('id');
+    })
   }
 
 }
